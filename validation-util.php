fc6e398c262c4658376e2cc0fdc017385dd1b396
Sarath <sarath.hacks@gmail.com>
2013-08-26 11:19:30 +0530

    Added is_email function

--- validation-util.php
+++ validation-util.php
@@ -1,4 +1,21 @@
 <?php
+/*
+*@Author  : Sarath
+*@PAckage : Validation-Util
+*@Created : 26-Aug-2013
+* Using reqular expression
+*/
+
+
+/*
+* is_email()
+* @param  :Email id (string)
+* @return : Return true if valid mail id
+*/
+function is_email($email){
+    return preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i', $email);
+  }
+  
 
 
 ?>
