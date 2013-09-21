ac9afc1908dd8a954575250e266093abf2b27045
Sarath <sarath.hacks@gmail.com>
2013-09-21 15:43:08 +0530

    Removed generateRandom

--- validation-util.php
+++ validation-util.php
@@ -25,17 +25,6 @@ function isAlpha($string){
     return ctype_alnum($string);
 }
 
-/*
-* Generate random string
-*/
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, strlen($characters) - 1)];
-    }
-    return $randomString;
-}
 
 
 ?>
