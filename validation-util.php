c4251979fa6fe9c14f5e772caa76d03ef2e7b10c
Sarath <sarath.hacks@gmail.com>
2014-01-13 12:00:52 +0530

    Update validation-util.php

--- validation-util.php
+++ validation-util.php
@@ -47,6 +47,10 @@ function valid_url($url){
 	return filter_var($url, FILTER_VALIDATE_URL);
 }
 
+function is_weburl($url=NULL){
+	return preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $url);
+}
+
 
 
 ?>
